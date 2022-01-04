24ee1f9dd1943ce0fe6b9f2c13f21f84c41b9b77
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2022-01-04 15:25:17 +0000

    Add outline for user selection

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,6 +4,11 @@ export default function AppHeader(): JSX.Element {
   return (
     <>
       <h1>Welcome to Cohort 3 Resource Catalogue</h1>
+      <div className="LoginSelector">
+        <select name="ChooseUser" id="ChooseUser">
+          <option value="">Select User</option>
+        </select>
+      </div>
       <NavBar />
     </>
   );
