dc013037ba258abbfeaecdad591b9d1b744a0891
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2022-01-04 16:42:32 +0000

    get all users and create options

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,12 +1,39 @@
 import NavBar from "./NavBar";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface IUser {
+  id: number;
+  name: string;
+  isFaculty: boolean;
+}
 
 export default function AppHeader(): JSX.Element {
+  const [userList, setUserList] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://resource-catalogue-be.herokuapp.com/users")
+      .then(function (response) {
+        setUserList(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const userOptions = userList.map((user) => (
+    <option value={user.id} key={user.id}>
+      {user.name}
+    </option>
+  ));
   return (
     <>
       <h1>Welcome to Cohort 3 Resource Catalogue</h1>
       <div className="LoginSelector">
         <select name="ChooseUser" id="ChooseUser">
           <option value="">Select User</option>
+          {userOptions}
         </select>
       </div>
       <NavBar />
