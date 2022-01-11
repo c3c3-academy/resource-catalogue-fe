21c06e60e242cbeca3a2ad543d84866096b6da2d
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2022-01-11 15:06:47 +0000

    added sample tests for test files

--- src/utils/containsTerm.test.ts
+++ src/utils/containsTerm.test.ts
@@ -1,3 +1,7 @@
 /* eslint-disable  @typescript-eslint/no-unused-vars */
 import { containsTerm } from "./containsTerm";
 /* eslint-enable  @typescript-eslint/no-unused-vars */
+
+test("fake test", () => {
+  expect("Hello, World!").toBe("Hello, World!");
+});
--- src/utils/isRecommended.test.ts
+++ src/utils/isRecommended.test.ts
@@ -1,3 +1,7 @@
 /* eslint-disable  @typescript-eslint/no-unused-vars */
 import { isRecommended } from "./isRecommended";
 /* eslint-enable  @typescript-eslint/no-unused-vars */
+
+test("fake test", () => {
+  expect("Hello, World!").toBe("Hello, World!");
+});
