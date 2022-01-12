566aa348ed8ac7a12c1e05864de1f97873cc55ef
Toye Akinoso <Toye.akinoso@yahoo.com>
2022-01-12 12:55:47 +0000

    added some cypress testing

---
+++ cypress/integration/list-of-resources.spec.js
@@ -0,0 +1,27 @@
+describe("all resources", () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("http://localhost:3000");
+  });
+
+  it("displays sixteen resource items by default", () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get(".SingleResource").should("have.length", 16);
+
+    cy.get(".SearchInput").click().type("react");
+
+    cy.get(".SingleResource").should("have.length", 3);
+  });
+
+  it("displays list of users", () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get(".LoginSelector").children().should("have.length", 32);
+  });
+});
