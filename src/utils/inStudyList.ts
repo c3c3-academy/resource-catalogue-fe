261ef314fcc71dd381297d188110ae14146d45f6
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2022-01-13 16:58:33 +0000

    change == into === and Boolean to boolean

--- src/utils/inStudyList.ts
+++ src/utils/inStudyList.ts
@@ -4,12 +4,12 @@ export function inStudyList(
   resourceId: number,
   userId: string | null,
   toStudyList: IToStudy[]
-): Boolean {
+): boolean {
   let filtered: IToStudy[] = [];
   if (userId !== null) {
     filtered = toStudyList.filter(
       (toStudy) =>
-        toStudy.userid == parseInt(userId) && toStudy.resourceid === resourceId
+        toStudy.userid === parseInt(userId) && toStudy.resourceid === resourceId
     );
   }
   return filtered.length === 0 ? false : true;
